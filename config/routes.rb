47f25de5f28f86b7401eaa67e008000d6ab9ab5a
Lauren <hyunalee625@gmail.com>
2020-07-28 10:19:48 -0400

    Routes file update

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
 
 
-  # root "static_pages#root"
+  root "static_pages#root"
 end
--- frontend/createtsy.jsx
+++ frontend/createtsy.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as sessionAPIUtil from './util/session_api_util'
 
-window.signup = sessionAPIUtil.signup;
-window.login = sessionAPIUtil.login;
-window.logout = sessionAPIUtil.logout;
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    window.signup = sessionAPIUtil.signup;
+    window.login = sessionAPIUtil.login;
+    window.logout = sessionAPIUtil.logout;
+    window.getState = store.getState
+    
+    
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Createtsy!</h1>, root);
-
-
 });
