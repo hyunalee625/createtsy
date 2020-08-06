69de3d0607c2b46ac2b53e4b79495d0d78bfca83
Lauren <hyunalee625@gmail.com>
2020-08-06 16:16:09 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ product1 = Product.create!(
     {
         product_name: "Tie-dye hoodie",
         description: "trendy",
-        price: 68,
+        price: $68,
     }
 )
 
@@ -50,7 +50,7 @@ product2 = Product.create!(
     {
         product_name: "Ceramic cup set",
         description: "perfect for gift",
-        price: 25,
+        price: $25,
     }
 )
 
@@ -61,7 +61,7 @@ product3 = Product.create!(
     {
         product_name: "Unicorn macarons",
         description: "Hand Painted with food coloring and vanilla extract!",
-        price: 42,
+        price: $42,
     }
 )
 
@@ -72,7 +72,7 @@ product4 = Product.create!(
     {
         product_name: "Granite keyboard",
         description: "Granite is a mega-keycap set - it offers coverage over a wide variety of both common and enthusiast mechanical keyboards. If you are looking for a consistent aesthetic that you will be able to use around the world with all of your keyboards, SA Granite is the ultimate set.",
-        price: 70,
+        price: $70,
     }
 )
 
@@ -83,7 +83,7 @@ product5 = Product.create!(
     {
         product_name: "Morton floor lamp",
         description: "Crafted of natural wood, our Morton Floor Lamp brings a casual touch to any space. Whether you want to complete a contemporary room or lend a touch of coastal style to a more traditional space, this lighting is the perfect choice for your home.",
-        price: 249,
+        price: $249,
     }
 )
 
@@ -94,7 +94,7 @@ product6 = Product.create!(
     {
         product_name: "Coffee table",
         description: 'Made from metal, its frame features a crisscross base in a gold finish for a hint of glam. Up above, the circular top is made from engineered wood with a durable laminate finish that’s versatile enough to mix with most color palettes. This compact 36" diameter piece is the perfect pick for smaller living rooms.',
-        price: 226,
+        price: $226,
     }
 )
 
@@ -105,7 +105,7 @@ product7 = Product.create!(
     {
         product_name: "Batman sanitizer holder",
         description: "The convenient adjustable silicone strap allows you to take germ fighting hand sanitizers with you wherever you go. Loop onto school bags, purses, gym bags and so much more. Fighting germs doesn't have to be boring.",
-        price: 2.50,
+        price: $2.50,
     }
 )
 
@@ -116,7 +116,7 @@ product8 = Product.create!(
     {
         product_name: "iPad Pro stand With Apple pencil holder",
         description: "The iPad Pro stand is a singular combination of high functionality and versatility, intuitive use, and a slender appearance. ",
-        price: 169,
+        price: $169,
     }
 )
 
@@ -127,7 +127,7 @@ product9 = Product.create!(
     {
         product_name: "Brass metal utensils",
         description: "This bold flatware isn't just for company; the stainless steel core makes it sturdy enough for everyday use while its unique shape makes every meal feel a little special. ",
-        price: 39.50,
+        price: $39.50,
     }
 )
 
@@ -138,7 +138,7 @@ product10 = Product.create!(
     {
         product_name: "RGB gaming mouse pad",
         description: "The micro textured hard surface allows for quick and smooth gliding.  A non-slip rubber base keeps the pad from moving even during fast paced gaming sessions.  The large size allows for plenty of movement.",
-        price: 30,
+        price: $30,
     }
 )
 
--- frontend/components/products/product_index_item.jsx
+++ frontend/components/products/product_index_item.jsx
@@ -7,9 +7,9 @@ const ProductIndexItem = ({ product }) => {
       <div>
         <ul className="product-index-item">
           <Link className="each-product" to={product.product_name}>
+            <img className="product-photo" src={product.photoUrl} alt="" />
             <li className="product-title">{product.product_name}</li>
             <li className="product-price">{product.price}</li>
-            <img src={product.photoUrl} alt="" />
           </Link>
         </ul>
       </div>
