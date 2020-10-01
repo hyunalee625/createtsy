339592d8a49f6a68ca3d2e8c35c1adf9d38ee44b
Lauren <hyunalee625@gmail.com>
2020-09-30 23:05:07 -0400

    Attempt to fix shopping cart

--- frontend/components/shopping_cart_items/shopping_cart_index_item.jsx
+++ frontend/components/shopping_cart_items/shopping_cart_index_item.jsx
@@ -3,96 +3,99 @@ import { withRouter } from "react-router-dom";
 import {Link} from 'react-router-dom'
 
 class ShoppingCartIndexItem extends React.Component {
-    constructor(props) {
-        super(props);
-        // debugger
-        this.state = {
-          id: this.props.item.id,
-          quantity: this.props.item.quantity,
-          };   
-          
-        this.state.item = this.props.item;
-    }
+  constructor(props) {
+    super(props);
+    // debugger
+    this.state = {
+      id: this.props.item.id,
+      quantity: this.props.item.quantity,
+    };
 
-    itemTotalPrice() {
-        const quantity = this.state.quantity;
-        const price = this.state.item.price;
-        return parseFloat(price * quantity).toFixed(2);
+    this.state.item = this.props.item;
+  }
 
-    }
+  itemTotalPrice() {
+    const quantity = this.state.quantity;
+    const price = this.props.item.price;
+    return parseFloat(price * quantity).toFixed(2);
+  }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        }), () => {
-            this.props.updateCartItem(this.state);
-        }
-    }
+  update(field) {
+    return (
+      (e) =>
+        this.setState({
+          [field]: e.currentTarget.value,
+        }),
+      () => {
+        this.props.updateCartItem(this.state);
+      }
+    );
+  }
 
-    emptyCart() {
-        return (
-            <div>
-                <h3>Your cart is empty.</h3>
-                <Link to="/">Discover something unique to fill it up</Link>
-            </div>
-        )
-    }
 
-    render() {
-        const {item} = this.state;
+  emptyCart() {
+    return (
+      <div>
+        <h3>Your cart is empty.</h3>
+        <Link to="/">Discover something unique to fill it up</Link>
+      </div>
+    );
+  }
 
-        return (
-          <div className="item-container">
-            <div className="index-item-left">
-              <Link
-                className="index-item-img"
-                to="{`/products/${item.product_id}`}"
-              >
-                <img src={item.photoURL}></img>
-              </Link>
-              <div className="index-info">
-                <Link
-                  className="index-item-name"
-                  to="{`/products/${item.product_id}`}"
-                >
-                  <label>{item.product_name}</label>
-                </Link>
-              </div>
-              <div className="index-item-remove">
-                <button
-                  onClick= {() => this.props.deleteCartItem(this.props.item.id)}
-                >
-                  Remove
-                </button>
-              </div>
-            </div>
+  render() {
+    const { item } = this.state;
 
-            <div className="index-item-right">
-              <select
-                className="item-quantity"
-                onChange={this.update("quantity")}
-                value={this.props.quantity}
-              >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-              <div className="index-item-price">
-                <div>${this.itemTotalPrice()}</div>
-                <div>(${item.price.toFixed(2)} each)</div>
-              </div>
-            </div>
+    return (
+      <div className="item-container">
+        <div className="index-item-left">
+          <Link
+            className="index-item-img"
+            to="{`/products/${item.product_id}`}"
+          >
+            <img src={item.photoURL}></img>
+          </Link>
+          <div className="index-info">
+            <Link
+              className="index-item-name"
+              to="{`/products/${item.product_id}`}"
+            >
+              <label>{item.product_name}</label>
+            </Link>
           </div>
-        );
-    }
+          <div className="index-item-remove">
+            <button
+              onClick={()=>this.props.deleteCartItem(this.props.item.id)}
+            >
+              Remove
+            </button>
+          </div>
+        </div>
 
+        <div className="index-item-right">
+          <select
+            className="item-quantity"
+            onChange={this.update("quantity")}
+            value={this.state.quantity}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <div className="index-item-price">
+            <div>${this.itemTotalPrice()}</div>
+            <div>(${item.price.toFixed(2)} each)</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(ShoppingCartIndexItem);
