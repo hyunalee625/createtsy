aefb3ff4f49474396ea811ea18117a3988d56f8d
Lauren <hyunalee625@gmail.com>
2020-10-07 12:35:09 -0400

    Add duplicate products to cartItem array

--- app/controllers/api/shopping_cart_items_controller.rb
+++ app/controllers/api/shopping_cart_items_controller.rb
@@ -2,48 +2,31 @@ class Api::ShoppingCartItemsController < ApplicationController
     # before_action :ensure_login
 
     def index
-        # @shopping_cart_items = current_user.shopping_cart_items
         @shopping_cart_items = ShoppingCartItem.all
-        # @products = Product.all
         render :index
     end
 
-    # def create
-    #     @shopping_cart_item = ShoppingCartItem.find_by(buyer_id: params[:cartItem][:buyer_id], product_id: params[:cartItem][:product_id])
-
-    #     if @shopping_cart_item
-    #         @shoppinog_cart_item.quantity += 1
-            # @max_quantity = false
-            # if @shopping_cart_item.quantity > 10
-            #     @max_quantity = true
-            #     @shopping_cart_item.quantity = 10 # capping quantity to 10
-            # end
-    #     else
-    #         @shopping_cart_item = ShoppingCartItem.new(shopping_cart_items_params)
-            
-    #         if @shopping_cart_item.save!
-    #             render :show
-    #         else
-    #             render json: @shopping_cart_item.errors.full_messages, status: 401
-    #         end
-    #     end
-
-    # end
 
     def create
-        @shopping_cart_item = ShoppingCartItem.new(shopping_cart_items_params)
+        @shopping_cart_item = ShoppingCartItem.find_by(product_id: params[:cartItem][:product_id])
 
-            if @shopping_cart_item.save!
-                @shopping_cart_item.quantity += 1
-                # @max_quantity = false
-                #     if @shopping_cart_item.quantity > 10
-                #         @max_quantity = true
-                #         @shopping_cart_item.quantity = 10 # capping quantity to 10
-                #     end
-                render :show
-            else
-                render json: @shopping_cart_item.errors.full_messages, status: 401
-            end
+        if @shopping_cart_item #if the item already exists
+            @shopping_cart_item.quantity += 1 # increment the quantity 
+             @max_quantity = false
+                    if @shopping_cart_item.quantity > 10
+                        @max_quantity = true
+                        @shopping_cart_item.quantity = 10 # capping quantity to 10
+                    end
+        else # create a new shopping cart item
+            @shopping_cart_item = ShoppingCartItem.new(shopping_cart_items_params)
+        end
+
+
+        if @shopping_cart_item.save
+            render :show
+        else
+            render json: @shopping_cart_item.errors.full_messages, status: 401
+        end
     end
 
     def update
